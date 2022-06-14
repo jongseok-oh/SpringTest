eae9eefb6580ea0d3f16eb95ed8f90c56cb8aa03
JongKing <hynb14@gmail.com>
2022-06-14 22:14:31 +0900

    Bean 다양하게 조회

---
+++ core/src/main/java/com/hihi/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,17 @@
+package com.hihi.core.discount;
+
+import com.hihi.core.member.Grade;
+import com.hihi.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    final Integer discountRatePercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP){
+            return price * discountRatePercent / 100;
+        }
+        return 0;
+    }
+}
